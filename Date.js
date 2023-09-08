763583d06347dda60bccbd9e274e1797aa7d2450
Ifly2sky <tinhau24@gmail.com>
2023-09-08 08:23:50 +0300

    js

---
+++ Date.js
@@ -0,0 +1,3 @@
+var date = new Date()
+var birthday = new Date(2006, 8, 24)
+console.log(date.getFullYear()-birthday.getFullYear())
---
+++ aritmeettisetOperaattorit.js
@@ -0,0 +1,3 @@
+var hinta = 69
+var alennusProsentti = 25
+console.log(25/100 * 69)
